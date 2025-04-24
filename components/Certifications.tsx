98b8dcb290bd20a22de1edc068c18aaa21b956ab
ayush pandey <ayushpandey@ayushs-MacBook-Air-6.local>
2025-04-24 09:37:28 +0530

    hi

--- components/Certifications.tsx
+++ components/Certifications.tsx
@@ -6,7 +6,7 @@ const Certifications = () => {
       <h2 className="text-2xl font-bold mb-6 text-blue-600">Certifications</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
         <CertificationCard
-          img="/img/certi4.png"
+          img="/img/certi1.png"
           title="Full Stack Web Dev"
           link="https://www.udemy.com/certificate/UC-bdc3896d-da1d-47b5-bb61-16141ac81e6e/"
         />
